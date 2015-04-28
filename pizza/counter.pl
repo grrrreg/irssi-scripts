9ef9ed82d0df9691603d32fb33e470541dc1b97f
Mike Jones <mike@netsplit.org.uk>
2015-04-28 22:57:23 +0100

    Update counter.pl

--- pizza/counter.pl
+++ pizza/counter.pl
@@ -6,65 +6,64 @@ use Data::Dumper;
 
 $VERSION = "1.0";
 %IRSSI = ( 
-        authors => 'Mike Jones',
-        contact => 'mike@snoonet.org',
-        name => 'Pizza Counter',
-        description => '#metal pizza counter incrementer',
-        license => 'MIT',
-        url => 'https://github.com/n7st/irssi-scripts',
+	authors => 'Mike Jones',
+	contact => 'mike@snoonet.org',
+	name => 'Pizza Counter',
+	description => '#metal pizza counter incrementer',
+	license => 'MIT',
+	url => 'https://github.com/n7st/irssi-scripts',
 );
 
 my @admins = ( 
-        'Mike@snoonet/admin/Netsplit',
-        'qdwach@user/Qdwach',
-        'brutal_n00d@user/BrutalN00dle'
+	'Mike@snoonet/admin/Netsplit',
+	'qdwach@user/Qdwach',
+	'brutal_n00d@user/BrutalN00dle'
 );
 
 my $running = 0;
 
 sub check_channel ($$) {
-        my ($channel, $target_channel) = @_; 
+	my ($channel, $target_channel) = @_; 
 
-        if ($channel->{name} eq $target_channel) {
-                return $channel->{topic};
-        }   
+	if ($channel->{name} eq $target_channel) {
+		return $channel->{topic};
+	}   
 
-        return 0;
+	return 0;
 }
 
 sub set_new_topic ($$$) {
-        my ($channel, $topic, $server) = @_; 
-        my ($count) = $topic =~ /:\ (\d+)/;
-        my $new_count = $count + 1;
-        $topic =~ s/$count/$new_count/;
+	my ($channel, $topic, $server) = @_; 
+	my ($count) = $topic =~ /:\ (\d+)/;
+	my $new_count = $count + 1;
+	$topic =~ s/$count/$new_count/;
 
-        $server->command("topic $channel $topic");
+	$server->command("topic $channel $topic");
 }
 
 sub handle_cmd ($$$$$) {
-        my ($server, $msg, $nick, $address, $target) = @_; 
-        my $current_topic;
+	my ($server, $msg, $nick, $address, $target) = @_; 
+	my $current_topic;
 
-        if ($msg ne "pizza++" || $running == 1) {
-                return 0;
-        }   
+	if ($msg ne "pizza++" || $running == 1) {
+		return 0;
+	}   
 
-        $running = 1;
+	$running = 1;
 
-        if ($address ~~ @admins) {
-                foreach (Irssi::channels()) {
-                        if ($current_topic = check_channel($_, $target)) {
-							last;
-                        }   
-                }   
-        }   
+	if ($address ~~ @admins) {
+		foreach (Irssi::channels()) {
+			if ($current_topic = check_channel($_, $target)) {
+				last;
+			}   
+		}   
+	}   
 
-        if ($current_topic) {
-                set_new_topic($target, $current_topic, $server);
-        }   
+	if ($current_topic) {
+		set_new_topic($target, $current_topic, $server);
+	}   
 
-        $running = 0;
+	$running = 0;
 }
 
 Irssi::signal_add('message public', 'handle_cmd');
-
